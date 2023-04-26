c019c47f69ff59e0580b08193c4234a8cbb237df
Marsel Sattarov <fsc-3@tages.ru>
2023-04-26 17:44:00 +0500

    create delete modal "editModal"

--- client/js/editClient.js
+++ client/js/editClient.js
@@ -1,3 +1,4 @@
+import { deleteClientModal } from "./createDeleteModal.js";
 import { createClientsForm } from "./createModalForm.js";
 
 export const editClientModal = (data) => {
@@ -14,9 +15,33 @@ export const editClientModal = (data) => {
    createForm.modalTitle.textContent = 'Изменить данные';
    createForm.cancelBtn.textContent = 'Удалить клиента';
 
+   createForm.cancelBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const deleteModal = deleteClientModal();
+      document.body.append(deleteModal.deleteModal);
+
+      import('./clientsApi.js').then(({ deleteClientItem }) => {
+         deleteModal.deleteModalDelete.addEventListener('click', () => {
+            deleteClientItem(data.id);
+            document.getElementById(data.id).remove();
+         });
+      });
+   });
+
+   createForm.modalClose.addEventListener('click', () => {
+      editModal.remove();
+   })
+
    createForm.modalTitle.append(titleId);
    editModalContent.append(createForm.modalClose, createForm.modalTitle, createForm.form);
    editModal.append(editModalContent);
+
+   document.addEventListener('click', (e) => {
+      if (e.target === editModal) {
+         editModal.remove();
+      }
+   });
 
    return {
       editModal,
