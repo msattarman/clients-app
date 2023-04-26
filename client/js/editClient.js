a2996d355cb0661e9288c4dcc7aed580c062ec65
Marsel Sattarov <fsc-3@tages.ru>
2023-04-26 16:58:19 +0500

    create id for client

--- client/js/editClient.js
+++ client/js/editClient.js
@@ -4,13 +4,17 @@ export const editClientModal = (data) => {
    const editModal = document.createElement('div');
    const editModalContent = document.createElement('div');
    const createForm = createClientsForm();
+   const titleId = document.createElement('span');
 
+   titleId.classList.add('modal__id');
    editModal.classList.add('modal-edit', 'site-modal', 'modal-active');
    editModalContent.classList.add('edit-modal__content', 'site-modal__content', 'modal-active');
 
+   titleId.textContent = 'ID: ' + data.id.substr(0, 6);
    createForm.modalTitle.textContent = 'Изменить данные';
    createForm.cancelBtn.textContent = 'Удалить клиента';
 
+   createForm.modalTitle.append(titleId);
    editModalContent.append(createForm.modalClose, createForm.modalTitle, createForm.form);
    editModal.append(editModalContent);
 
